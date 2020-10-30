2a82a1a8a57d8029398b2698cb2d2a21ecd69d01
Bartek <bartosz.walawender@gmail.com>
2020-10-30 16:36:59 +0100

    .

--- codeRepo/tests/test_semanalyzer.py
+++ codeRepo/tests/test_semanalyzer.py
@@ -15,12 +15,14 @@ class TestSemanticAnalyzer(TestCase):
         stream = CommonTokenStream(lexer)
         return krlParser(stream)
 
-    def test_visit_literal(self):
+    # TODO >> write test for other types of literals and rename test functions
+    def test_visit_float_literal(self):
         test_string = "2.3"
         parser = self.parser_from_string(test_string)
         literal_node = parser.literal()
 
         result = literal_node.accept(self.sem_analyzer)
+        self.assertIsInstance(result, float)
         self.assertEqual(result, 2.3)
 
 
