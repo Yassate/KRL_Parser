c61697f3e6730b0bcfc61663102cf43cb03c0a55
Bartek <bartosz.walawender@gmail.com>
2020-11-01 20:27:38 +0100

    Added tests for forward kinematics coordinates. Angles are wrong, need to change calculation method.

--- codeRepo/test/test_semanalyzer.py
+++ codeRepo/test/test_semanalyzer.py
@@ -15,15 +15,32 @@ class VisitorTester(TestCase):
         stream = CommonTokenStream(lexer)
         return krlParser(stream)
 
+
 class VisitLiteralTest(VisitorTester):
-    def test_visit_float_literal_returns_float(self):
-        test_string = "2.3"
+    def _result_from_string(self, test_string):
         parser = self.parser_from_string(test_string)
         literal_node = parser.literal()
+        return literal_node.accept(self.sem_analyzer)
 
-        result = literal_node.accept(self.sem_analyzer)
+    def test_visit_float_literal_returns_float(self):
+        test_string = "2.38765"
+        result = self._result_from_string(test_string)
+
         self.assertIsInstance(result, float)
-        self.assertEqual(result, 2.3)
+        self.assertEqual(result, 2.38765)
 
+    def test_visit_int_literal_returns_int(self):
+        test_string = "3867564"
+        result = self._result_from_string(test_string)
+
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, 3867564)
+
+    def test_visit_bool_literal_returns_bool(self):
+        test_string = "tRUe"
+        result = self._result_from_string(test_string)
+
+        self.assertIsInstance(result, bool)
+        self.assertEqual(result, True)
 
 
